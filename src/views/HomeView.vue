d251fedc7a715955db671726a35a45f3b4d172c6
APHOTHAWATS <47128431+robinvansurat@users.noreply.github.com>
2023-07-13 18:04:58 +0700

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,8 @@
 <script setup>
-import banner from "../assets/image 2.png"
 </script>
 
 <template>
     <div class="banner">
-        <!-- Content inside the banner -->
-        <!-- <h1>Welcome to my website</h1>
-            <p>Explore and enjoy!</p> -->
     </div>
     <div class="container-fluid">
         <div class="row">
@@ -27,7 +23,6 @@ import banner from "../assets/image 2.png"
         </div>
     </div>
     <div class="banner2">
-        <!-- Content inside the banner -->
         <div class="container row">
             <div class="col-6 row banner2_text">
                 <div class="col-12">
@@ -44,6 +39,7 @@ import banner from "../assets/image 2.png"
     </div>
     <div class="container-fluid">
         <div class="row">
+
             <div class="col-12">
                 <div class="row">
                     <div class="col-4 text-center mb-4">
@@ -58,18 +54,97 @@ import banner from "../assets/image 2.png"
                 </div>
             </div>
 
+            <div class="col-12">
+                <div class="text-center mt-5">
+                    <p class="txt-new">New Available</p>
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="container align-items-center">
+                    <div class="row">
+                        <div class="col-4 text-center ">
+                            <img class="m-3" src="@/assets/image 9.png" />
+                        </div>
+                        <div class="col-4 text-center">
+                            <img class="m-3" src="@/assets/image 10.png" />
+                        </div>
+                        <div class="col-4 text-center">
+                            <img class="m-3" src="@/assets/image 11.png" />
+                        </div>
+                        <div class="col-4 text-center">
+                            <img class="m-3" src="@/assets/image 12.png" />
+                        </div>
+                        <div class="col-4 text-center">
+                            <img class="m-3" src="@/assets/image 13.png" />
+                        </div>
+                        <div class="col-4 text-center">
+                            <img class="m-3" src="@/assets/image 14.png" />
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="col-12">
+                <div class="d-flex justify-content-center mt-5">
+                    <a href="/products">
+                        <button type="button" class="btn btn-more position-relative">
+                            MORE <svg xmlns="http://www.w3.org/2000/svg" width="17" height="27" viewBox="0 0 17 27"
+                                fill="none">
+                                <path d="M1.56 1.85986L15.14 13.4999L1.56 25.1399" stroke="#79BCCF" stroke-width="3"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                            </svg>
+                        </button>
+                    </a>
+                </div>
+            </div>
+
         </div>
     </div>
-    <!-- <img alt="" class="banner" src="@/assets/image 2.png" /> -->
+    <div class="footter">
+
+    </div>
 </template>
 
 <script>
-export default {
-    name: 'Banner',
-};
+// export default {
+//     name: 'Banner',
+// };
 </script>
 
 <style scoped>
+.footter {
+    width: 1440px;
+    height: 216px;
+    flex-shrink: 0;
+}
+
+.btn-more {
+
+    width: 446.2px;
+    height: 97px;
+    flex-shrink: 0;
+
+    border-radius: 100px;
+    background: #FFF;
+    box-shadow: 0px 7px 20px 0px rgba(121, 188, 207, 0.40);
+
+    color: #79BCCF;
+    font-family: Montserrat;
+    font-size: 40px;
+    font-style: normal;
+    font-weight: 700;
+    line-height: normal;
+    letter-spacing: 2px;
+    text-transform: uppercase;
+}
+
+.txt-new {
+    font-size: 8em;
+    letter-spacing: 0.3em;
+}
+
 .banner {
     background-image: url('../assets/image 2.png');
     background-size: cover;
@@ -77,10 +152,7 @@ export default {
     width: 100%;
     height: 844px;
     margin: auto;
-    /* Set the desired height for your banner */
     color: #fff;
-    /* Set the text color */
-    /* text-align: center; */
 }
 
 
@@ -91,18 +163,18 @@ export default {
     width: 100%;
     height: 754px;
     margin: auto;
-    /* Set the desired height for your banner */
     color: #fff;
-    /* Set the text color */
-    /* text-align: center; */
 }
+
 .banner_text_h2 {
     font-size: 2.5rem;
 }
+
 .banner_text_h1 {
     font-size: 4.5rem;
     font-weight: 600;
 }
+
 .banner2_text {
     margin-top: 10%;
     margin-left: 10%;
---
+++ src/views/Products.vue
@@ -0,0 +1,145 @@
+<template>
+    <div class="container">
+        <div class="row">
+
+            <div class="col-4" v-for="card in cards" :key="card.id">
+                <div class="card" style="width: 18rem;">
+                    <img :src="card.image" class="card-img-top" :alt="card.title" />
+                    <div class="card-body">
+                        <h5 class="card-title">{{ card.title }}</h5>
+                        <p class="card-text">{{ card.description }}</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</template>
+
+<script>
+import image1 from '@/assets/image 3.png';
+import image2 from '@/assets/image 5.png';
+
+export default {
+    name: 'CardList',
+    data() {
+        return {
+            cards: [
+                {
+                    id: 1,
+                    image: image1,
+                    title: 'Card 1',
+                    description: 'Some quick example text for Card 1',
+                },
+                {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                },
+                {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                }, {
+                    id: 2,
+                    image: image2,
+                    title: 'Card 2',
+                    description: 'Some quick example text for Card 2',
+                },
+            ],
+        };
+    },
+};
+</script>
+
+
+
+<style scoped></style>
